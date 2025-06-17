d0a5df24d6126b654bd1407f7ba6592495168421
Pranav0786 <pranavsurve.0786@gmail.com>
2025-06-17 11:14:16 +0530

    q

---
+++ 28_DP_Striver/1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void pranav()
+{
+
+}
+
+int main()
+{
+    int t ;
+    cin >> t ;
+    while( t-- )
+        pranav() ;
+    return 0 ;
+}
